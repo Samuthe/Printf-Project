de24accc6cf175660e334589e3468c2d57902805
Samuthe <ekatisamuel@gmail.com>
2022-05-25 17:06:34 +0300

    match_cases.c

--- match_cases.c
+++ match_cases.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * match_specifier - matches specifier function for each conversion specifier
- * also handles modifiers of short and long
- * @inv: the arguments inventory with most commonly used arguments
- * Return: pointer to the helper function or NULL
- */
+* match_specifier - matches specifier function for each conversion specifier
+* also handles modifiers of short and long
+* @inv: the arguments inventory with most commonly used arguments
+* Return: pointer to the helper function or NULL
+*/
 void (*match_specifier(inventory_t *inv))(inventory_t *)
 {
     int j, i = 0;
@@ -12,6 +12,7 @@ void (*match_specifier(inventory_t *inv))(inventory_t *)
     static const char modifiers[] = "hl+#";
     static matches_t specifiers_list[] = {
         {'d', p_int}, {'i', p_int}, {'x', p_lowhex}, {'X', p_uphex}, {'o', p_oct}, {'u', p_ulongint}, {'c', p_char}, {'s', p_string}, {'%', p_percent}, {'b', p_binary}, {'p', p_pointer}, {'r', p_rev_string}, {'R', p_rot13}, {'S', p_string_hex}, {'\0', NULL}};
+
     static matches_t modifiers_long[] = {
         {'d', p_longint}, {'i', p_longint}, {'x', p_longlowhex}, {'X', p_longuphex}, {'o', p_longoct}, {'u', p_ulongint}, {'\0', NULL}};
 
